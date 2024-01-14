d4de55addc9a53673b01eb7b19252d81a8cfb9e0
Rei3373 <152368897+Rei3373@users.noreply.github.com>
2024-01-14 18:14:40 +0900

    LESSON04-2

--- src/main/java/com/example/controller/LibraryController.java
+++ src/main/java/com/example/controller/LibraryController.java
@@ -7,25 +7,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Library;
 import com.example.service.LibraryService;
+import com.example.service.LogService;
 
 @Controller
 @RequestMapping("library")
 public class LibraryController {
 
 	private final LibraryService libraryService;
+	private final LogService logService;
 
     @Autowired
-    public LibraryController(LibraryService libraryService) {
+    public LibraryController(LibraryService libraryService, LogService logService) {
         this.libraryService = libraryService;
+        this.logService = logService;
     }
 
     @GetMapping
     public String index(Model model) {
-        List<Library> libraries = this.libraryService.findAll();
+        List<Library> libraries = this.libraryService.findAllLibrary();
         model.addAttribute("libraries", libraries);
         return "library/index";
+    }
+    
+    @GetMapping("/borrow")
+    public String borrowingForm(@RequestParam("id")Integer id, Model model) {
+    	Library library = libraryService.findById(id).get();
+    	return "/library/borrowingForm";
     }
 }
