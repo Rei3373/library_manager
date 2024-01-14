393ff313aba1790d4802000060c042aac378bb32
Rei3373 <152368897+Rei3373@users.noreply.github.com>
2024-01-14 18:18:14 +0900

    LESSON04-3

--- src/main/java/com/example/controller/LibraryController.java
+++ src/main/java/com/example/controller/LibraryController.java
@@ -1,17 +1,23 @@
 package com.example.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Library;
+import com.example.entity.Log;
 import com.example.service.LibraryService;
 import com.example.service.LogService;
+import com.example.service.LoginUser;
 
 @Controller
 @RequestMapping("library")
@@ -38,4 +44,22 @@ public class LibraryController {
     	Library library = libraryService.findById(id).get();
     	return "/library/borrowingForm";
     }
+    
+    @PostMapping("/borrow")
+	public String borrow(@RequestParam("id") Integer id, @RequestParam("return_due_date") String returnDueDate, @AuthenticationPrincipal LoginUser loginUser) {
+		Library library = libraryService.findById(id).get();
+		library.setUserId(loginUser.getUser().getId());
+		libraryService.update(library);
+
+		Log log = new Log();
+		log.setLibraryId(id);
+		log.setUserId(loginUser.getUser().getId());
+		log.setRentDate(LocalDateTime.now());
+		log.setReturnDate(null);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		log.setReturnDueDate(LocalDateTime.parse(returnDueDate + " 00:00:00", formatter));
+		log.setReturnDate(null);
+		logService.create(log);
+		return "redirect:/library";
+	}
 }
